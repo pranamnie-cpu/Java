b0039bb5c65d8d368e6668a1978e165d0ce5a456
PRANAM R <pranamnie@gmail.com>
2026-09-22 23:00:11 +0530

    Update Basic.java
    class are cretaed on same file

--- Basic.java
+++ Basic.java
@@ -1,5 +1,26 @@
 class Basic{
   public static void main(String [] args){
     System.out.println("Hellow Java");
+
+    Student s1 = new Student("Pranam", "1MS23CS001", 95.5);
+    s1.display();
   }
 }
+
+class Student(
+  String name;
+  String usn;
+  double mark;
+
+  void Student(String name,String usn;double mark)
+    {
+    this.name=name;
+  this.usn=usn;
+  this.mark=mark;
+    }
+  void display(){
+    System.out.println("Name: " + name);
+    System.out.println("USN: " + usn);
+    System.out.println("Mark: " + mark);
+  }
+)
