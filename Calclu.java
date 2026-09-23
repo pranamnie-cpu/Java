32ca1c283d00a68dd25d419993446f5bb51a17ac
PRANAM R <pranamnie@gmail.com>
2026-09-23 22:19:08 +0530

    Create Calclu.java
    schema

---
+++ Calclu.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Calclu{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(Systen.in);
+    
+    System.out.println("Take A :");
+    int a=sc.nextInt();
+
+    System.out.println("Take B :");
+    int b=sc.nextInt();
+  }
+}
