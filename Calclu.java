7b2445393534e38858052d1c8abad8109842f83d
PRANAM R <pranamnie@gmail.com>
2026-09-23 22:22:45 +0530

    Update Calclu.java
    Addition is done

--- Calclu.java
+++ Calclu.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 class Calclu{
   public static void main(String args[]){
-    Scanner sc=new Scanner(Systen.in);
+    Scanner sc=new Scanner(System.in);
     
     System.out.println("Take A :");
     int a=sc.nextInt();
 
     System.out.println("Take B :");
     int b=sc.nextInt();
+
+    System.out.println("Sum :" +(a+b));
+    
   }
 }
